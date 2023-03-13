0808ae2c4e7ed82080de7c25136a59ee8b619592
Ernest Ho <ernest.heh@gmail.com>
2023-03-13 16:47:41 +0800

    Added associations for bookings and listings to User model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,4 @@
 class Listing < ApplicationRecord
-  belongs_to :owner, class_name: "User", foreign_key: 'owner_id'
+  has_many :bookings
+  belongs_to :owner, class_name: "User", foreign_key: "owner_id"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_080615) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_082947) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "listing_id", null: false
+    t.integer "total", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listing_id"], name: "index_bookings_on_listing_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "listings", force: :cascade do |t|
     t.string "name", null: false
