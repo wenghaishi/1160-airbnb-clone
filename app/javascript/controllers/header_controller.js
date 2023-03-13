5ce834b9879304d2dfc547663f8df6ffa8a7cdd6
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-03-13 16:58:17 +0800

    created header dropdown logic

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -5,10 +5,22 @@ export default class extends Controller {
   static targets = ['openUserMenu'];
 
   connect() {
+    console.log(this.openUserMenuTarget);
     this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu)
+    console.log("hi")
   }
 
   toggleDropdownMenu() {
     toggle(document.getElementById('menu-dropdown-items'));
+    console.log("hi")
   }
 }
+
+// import { Controller } from "@hotwired/stimulus"
+
+// // Connects to data-controller="header"
+// export default class extends Controller {
+//   connect() {
+//   }
+// }
+
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import HeaderController from "./header_controller"
+application.register("header", HeaderController)
+
+import TestHeaderController from "./test_header_controller"
+application.register("test-header", TestHeaderController)
