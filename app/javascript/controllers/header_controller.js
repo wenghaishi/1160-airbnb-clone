5aea1cea005a2340bd03cd7623f83f563d47cfed
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-03-13 16:38:06 +0800

    created dropdown function for header

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -1,7 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
+import { toggle } from 'el-transition'
 
 export default class extends Controller {
+  static targets = ['openUserMenu'];
+
   connect() {
-    this.element.textContent = "Hello World!"
+    this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu)
+  }
+
+  toggleDropdownMenu() {
+    toggle(document.getElementById('menu-dropdown-items'));
   }
 }
