090db30aa1cde4564bfc6a00894121e55cb6f5ce
shaun lee <sleewenyu@gmail.com>
2023-03-14 18:04:13 +0800

    added incomplete reservation form, refactored to partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "listings#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :listings 
+  resources :listings
   # Defines the root path route ("/")
   # root "articles#index"
 end
