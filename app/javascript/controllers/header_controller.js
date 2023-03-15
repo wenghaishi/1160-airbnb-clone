8e32ddbab1d5f5f0c91973a62192488b9e5af601
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-03-15 09:40:00 +0800

    commit to go to new branch

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -5,22 +5,12 @@ export default class extends Controller {
   static targets = ['openUserMenu'];
 
   connect() {
+    console.log("hi")
     console.log(this.openUserMenuTarget);
     this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu)
-    console.log("hi")
   }
 
   toggleDropdownMenu() {
     toggle(document.getElementById('menu-dropdown-items'));
-    console.log("hi")
   }
 }
-
-// import { Controller } from "@hotwired/stimulus"
-
-// // Connects to data-controller="header"
-// export default class extends Controller {
-//   connect() {
-//   }
-// }
-
