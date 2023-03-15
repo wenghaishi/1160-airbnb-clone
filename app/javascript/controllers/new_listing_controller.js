86fc0eb8f83ff306fb45dfaee113ea8947f606c5
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-03-15 14:08:53 +0800

    completed listing multi step form

--- app/javascript/controllers/new_listing_controller.js
+++ app/javascript/controllers/new_listing_controller.js
@@ -2,17 +2,31 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="new-listing"
 export default class extends Controller {
-  static targets = ['next'];
+  static targets = ["next", "next1"];
 
   
   connect() {
     console.log("hi")
     console.log(this.openUserMenuTarget);
     this.nextTarget.addEventListener('click', this.nextStep.bind(this))
+    this.next1Target.addEventListener('click', this.lastStep.bind(this))
   }
 
   nextStep() {
-    const item = document.getElementById('first-form');
-    item.classList.toggle('hidden');
+    const firstItem = document.getElementById('first-form');
+    const secondItem = document.getElementById('second-form');
+    const firstFooter = document.getElementById('firstFooter');
+    const secondFooter = document.getElementById('secondFooter');
+    firstFooter.classList.toggle('hidden');
+    secondFooter.classList.toggle('hidden');
+    firstItem.classList.toggle('hidden');
+    secondItem.classList.toggle('hidden');
+  }
+
+  lastStep() {
+    const lastItem = document.getElementById('last-form');
+    const secondItem = document.getElementById('second-form');
+    lastItem.classList.remove('hidden');
+    secondItem.classList.toggle('hidden');
   }
 }
