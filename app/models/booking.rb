b422f68d25594b3be6970f7ce1773a9c0966ea5c
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-03-16 09:13:13 +0800

    added has_one_attatched photo to modals

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :listing
   has_one :owner, class_name: "User", foreign_key: :owner_id, through: :listing
+  has_one_attached :photo
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,6 @@
 class Listing < ApplicationRecord
   has_many :bookings
   belongs_to :owner, class_name: "User", foreign_key: "owner_id"
+  has_one_attached :photo
+
 end
