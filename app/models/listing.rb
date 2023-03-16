a49b15dbbbbef655d2f1e16afcb670370b94d12e
Ernest Ho <ernest.heh@gmail.com>
2023-03-16 14:28:36 +0800

    added placeholder for map, added migration for lng lat values to listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,7 @@
 class Listing < ApplicationRecord
   has_many :bookings
   belongs_to :owner, class_name: "User", foreign_key: "owner_id"
-  has_many_attached :photo
+  has_many_attached :photos
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_16_001301) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_16_051836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_001301) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "img_url"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["owner_id"], name: "index_listings_on_owner_id"
   end
 
