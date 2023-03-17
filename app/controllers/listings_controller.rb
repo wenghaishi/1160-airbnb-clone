ba60a4fbeaaaf49d87b0eba8a9941ebde347c752
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-03-17 08:57:56 +0800

    commit to change branch

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -21,6 +21,7 @@ class ListingsController < ApplicationController
     @listing = Listing.new(listing_params)
     @listing.owner = current_user
     authorize @listing
+
     if @listing.save
       redirect_to listings_path
       # redirect_to @listing
